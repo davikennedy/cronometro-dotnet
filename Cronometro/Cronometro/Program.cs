c4df99db2d75cc1ec084d1bde1abaef44d32df38
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2023-01-22 22:57:16 -0300

    Criando estrutura inicial

---
+++ Cronometro/Cronometro/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Cronometro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cronômetro: ");
+
+            Start();
+            Console.ReadKey();
+        }
+
+        static void Start()
+        {
+            int time = 10;
+            int currentTime = 0;
+
+            while(currentTime != time)
+            {                
+                currentTime++;
+                Console.WriteLine(currentTime);
+            }
+        }
+    }
+}
